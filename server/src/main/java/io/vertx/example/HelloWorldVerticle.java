5f9133951d32e74e167081a9b3028f40a35c9ac3
Thiele, Michael <michael.thiele@saxsys.de>
2016-09-06 13:47:30 +0200

    server emits event bus messages via WebSocket

--- server/src/main/java/io/vertx/example/HelloWorldVerticle.java
+++ server/src/main/java/io/vertx/example/HelloWorldVerticle.java
@@ -1,16 +1,38 @@
 package io.vertx.example;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.sockjs.BridgeEventType;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
 /**
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 public class HelloWorldVerticle extends AbstractVerticle {
 
-  @Override
-  public void start() {
-    // Create an HTTP server which simply returns "Hello World!" to each request.
-    vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
-  }
+    @Override
+    public void start() {
+        final Router router = Router.router(vertx);
+        router.route("/eventbus/*").handler(eventBusHandler());
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
+        vertx.setPeriodic(1000, event -> vertx.eventBus().publish("yeah", "omg!"));
+    }
+
+    private SockJSHandler eventBusHandler() {
+        BridgeOptions options = new BridgeOptions()
+                .addOutboundPermitted(new PermittedOptions().setAddressRegex(".*"));
+        return SockJSHandler.create(vertx).bridge(options, event -> {
+            if (event.type() == BridgeEventType.SOCKET_CREATED) {
+                System.out.println("A socket was created");
+            }
+            event.complete(true);
+        });
+    }
 }
