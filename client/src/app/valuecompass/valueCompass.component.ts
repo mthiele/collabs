7d5efc5c0134686eb7993c4d42aa66298c3dc92c
Denny Israel <denny.israel@saxsys.de>
2016-09-12 13:35:33 +0200

    #147089: show correct value compass url and update site according to parameter

--- client/src/app/valuecompass/valueCompass.component.ts
+++ client/src/app/valuecompass/valueCompass.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Renderer, ElementRef} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 
 import {ValueCompass} from "../valueCompass";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'value-compass',  // <value-compass></value-compass>
@@ -9,16 +10,22 @@ import {ValueCompass} from "../valueCompass";
 })
 export class ValueCompassComponent implements OnInit {
 
+  private sub: Subscription;
+
   valueCompassId: string;
 
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.route.params.forEach((params: Params) => {
+    this.sub = this.route.params.subscribe(params => {
       let id = params['id'];
       console.log('showing value compass: ' + id);
       this.valueCompassId = id;
     });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
