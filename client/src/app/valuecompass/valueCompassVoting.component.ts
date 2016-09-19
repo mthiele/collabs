631c00759b2dde47de466777219dc6f65f62fa46
Stefan Illgen <stefan.illgen@saxsys.de>
2016-09-19 06:38:28 +0200

    show dimensions

--- client/src/app/valuecompass/valueCompassVoting.component.ts
+++ client/src/app/valuecompass/valueCompassVoting.component.ts
@@ -11,15 +11,13 @@ import {Dimension} from "../model/dimension";
 })
 export class ValueCompassVotingComponent implements OnInit {
   valueCompassId: string;
-  voting: Voting[] = [
-    new Voting(0, [
-      new Dimension("Fokus", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
-      new Dimension("Offenheit", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
-      new Dimension("Verpflichtung", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
-      new Dimension("Mut", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
-      new Dimension("Respekt", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)
-    ])
-  ];
+  voting: Voting = new Voting(0, [
+    new Dimension("Fokus", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
+    new Dimension("Offenheit", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
+    new Dimension("Verpflichtung", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
+    new Dimension("Mut", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0),
+    new Dimension("Respekt", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)
+  ]);
 
   private sub: Subscription;
 
